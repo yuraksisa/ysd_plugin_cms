ccf4ca134ae9de04481f4a62c712f221090b4ad8
yurak <yurak.sisa.dream@gmail.com>
2012-12-01 12:23:36 +0100

    views and block title

--- lib/sinatra/ysd_sinatra_view_management.rb
+++ lib/sinatra/ysd_sinatra_view_management.rb
@@ -7,6 +7,26 @@ module Sinatra
    
       def self.registered(app)
         
+        app.helpers do
+
+          def extract_view_arguments(arguments_page, arguments_no_page)
+
+            page = [params[:page].to_i, 1].max
+
+            arguments_start_at = if request.path_info.match /\/page\/\d+/
+                                   arguments_page
+                                 else            
+                                   arguments_no_page
+                                 end
+           
+            arguments = (x=request.path_info.split('/')).slice(arguments_start_at, x.length).join('/')
+             
+            return [page, arguments]
+
+          end
+
+        end
+
         #
         # View management page
         #
@@ -18,29 +38,67 @@ module Sinatra
           load_em_page('view_management'.to_sym, :view, false, :locals => locals)
 
         end
-               
+                       
         #
-        # Loads a view as a page
+        # Get a view page
         #
-        ["/:view_name",
-         "/:view_name/page/:page/*",
-         "/:view_name/*"].each do |path|
+        [ "/view/:view_name/?*",
+          "/view/:view_name/page/:page/?*"].each do |path| 
+          app.get path do
+
+            if view = ContentManagerSystem::View.get(params[:view_name])
+
+              page, arguments = extract_view_arguments(5, 3)
+              
+              begin
+                CMSRenders::ViewRender.new(view, self).render(page, arguments)
+              rescue ContentManagerSystem::ViewArgumentNotSupplied
+                status 404
+              end              
+            else
+              status 404          
+            end
+          
+          end
+
+        end
+
+
+        #
+        # Gets a view preview
+        #
+        ["/view/preview/:view_name/?*",
+         "/view/preview/:view_name/page/:page/?*"].each do |path|
+          
+          app.get path do
+        
+            if view = ContentManagerSystem::View.get(params[:view_name])      
+              page, arguments = extract_view_arguments(6, 4)   
+              begin
+                CMSRenders::ViewRender.new(view, self).render(1, arguments)
+              rescue ContentManagerSystem::ViewArgumentNotSupplied
+                status 404
+              end
+            else
+              "Sorry, the views #{params[:view_name]} does not exist"
+            end
+        
+          end
+          
+        end
+      
+        #
+        # Loads a view as a page by its url
+        #
+        ["/:view_name/?*",
+         "/:view_name/page/:page/*"].each do |path|
         
           app.get path do
           
             view = ContentManagerSystem::View.first(:url => params['view_name'])
             pass unless view
-
-            page = params[:page]
-            arguments_start_at = if request.path_info.match /\/page\/\d+/
-                                   4
-                                 else            
-                                   2
-                                 end
-
-            arguments = (x=request.path_info.split('/')).slice(arguments_start_at, x.length).join('/')
-
-            page = [params[:page].to_i, 1].max 
+           
+            page, arguments = extract_view_arguments(4, 2)
 
             # Creates a content using the view information
             begin
@@ -56,49 +114,7 @@ module Sinatra
 
           end
         end
-        
-        #
-        # Get a view page
-        #
-        app.get "/view/:view_name/page/:page/?*" do 
 
-          if view = ContentManagerSystem::View.get(params[:view_name])
-            arguments = (x=request.path_info.split('/')).slice(4,x.length).join('/')
-            begin
-              CMSRenders::ViewRender.new(view, self).render(params[:page].to_i, arguments)
-            rescue ContentManagerSystem::ViewArgumentNotSupplied
-              status 404
-            end              
-          else
-            status 404          
-          end
-
-        end
-
-
-        #
-        # Gets a view preview
-        #
-        ["/view/preview/:view_name",
-         "/view/preview/:view_name/*"].each do |path|
-          
-          app.get path do
-        
-            if view = ContentManagerSystem::View.get(params[:view_name])         
-              arguments = (x=request.path_info.split('/')).slice(4,x.length).join('/')
-              begin
-                CMSRenders::ViewRender.new(view, self).render(1, arguments)
-              rescue ContentManagerSystem::ViewArgumentNotSupplied
-                status 404
-              end
-            else
-              "Sorry, the views #{params[:view_name]} does not exist"
-            end
-        
-          end
-          
-        end
-      
       end
     
     end #ViewManagement
